d3bac5a12a78f7a7bc6b88073534803b496cad02
Yahya Ali <yahya.ali@tintash.com>
2019-07-08 15:47:28 +0500

    Added Activity Module
    What: With @ContributesInjection
    Why: To allow dagger to inject modules into any activity specified in the module

--- app/src/main/java/com/blackbox/baseproject/shared/application/AppComponent.kt
+++ app/src/main/java/com/blackbox/baseproject/shared/application/AppComponent.kt
@@ -1,23 +1,35 @@
 package com.blackbox.baseproject.shared.application
 
 
+import com.blackbox.baseproject.shared.activities.ActivityBuilderModule
 import com.blackbox.baseproject.shared.viewModel.ViewModelModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 
+/**
+ *
+ * We mark this interface with the @Component annotation.
+ * And we define all the modules that can be injected.
+ * Note that we provide AndroidSupportInjectionModule.class
+ * here. This class was not created by us.
+ * It is an internal class in Dagger 2.10.
+ * Provides our activities and fragments with given module.
+ *
+ */
 @Component(
     modules = [AndroidSupportInjectionModule::class,
         AppModule::class,
-    ViewModelModule::class])
+        ViewModelModule::class,
+        ActivityBuilderModule::class,
+        AndroidSupportInjectionModule::class]
+)
 interface AppComponent : AndroidInjector<BlackBoxApplication> {
     @Component.Builder
     interface Builder {
         @BindsInstance
         fun application(application: BlackBoxApplication): Builder
-
-
         fun build(): AppComponent
     }
 }
